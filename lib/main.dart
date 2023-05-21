6677a0c2e242b83fedf14fafa49b20c38660b406
Asma-Ha <ja_hamidi@esi.dz>
2023-05-21 09:32:45 +0100

    camera in the background when app is started, main thread

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
+import 'package:mobile_1/services/camera_handler.dart';
 import 'package:provider/provider.dart';
 import 'viewmodels/DrinksListViewModel.dart';
 import 'views/HomePageView.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  CameraHandler cameraHandler = CameraHandler();
+  try {
+    cameraHandler.initializeCamera();
+    runApp(MyApp());
+  } catch(e) {
+    print("Camera exception ${e.message}");
+  }
+
 }
 
 class MyApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import geolocator_apple
+import location
 import path_provider_foundation
-import sqflite
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
